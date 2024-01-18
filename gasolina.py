e8970bb51bb75b4e88d3930b1fdf6d4f0fe18699
${GITHUB_USER} <${GITHUB_MAIL}>
2024-01-18 20:18:42 -0300

    atualização dos arquivos

---
+++ gasolina.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+tabela = pd.read_csv("gasolina.csv")
+
+with sns.axes_style('whitegrid'):
+  grafico = sns.lineplot(data=tabela, x="dia", y="venda", palette="bright")
+  grafico.set(title='Preço médio da Gasolina em SP - Julho/21', xlabel='Dia', ylabel='Preço médio (R$)');
+
+grafico.legend(["Vendas"], loc='upper right', bbox_to_anchor=(1.25, 1))
